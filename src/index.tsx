cdf66c28247017b71e17a169b057edf4b92505ca
William Chang <wilicw1346@gmail.com>
2022-09-27 10:02:15 +0800

    Add fluentui

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <FluentProvider theme={webLightTheme}>
+      <App />
+    </FluentProvider>
   </React.StrictMode>
 );
 
