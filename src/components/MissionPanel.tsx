d9fd6b103d1abb070b37e6b68c957196252423cb
William Chang <wilicw1346@gmail.com>
2022-09-30 15:03:47 +0800

    MissionPanel component

---
+++ src/components/MissionPanel.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Card } from '@fluentui/react-components/unstable';
+import {
+  Title2,
+  Subtitle1,
+  Body1,
+  CompoundButton,
+  Textarea,
+  Label,
+  Button,
+} from '@fluentui/react-components';
+import { Separator } from '@fluentui/react/lib/Separator';
+
+interface MissionPanelProps {
+  missionName: string;
+  rocketType: string;
+  motorType: string;
+  avionicType: string;
+  message: string;
+}
+
+function MissionPanel({ missionName, rocketType, motorType, avionicType, message }: MissionPanelProps) {
+  return (
+    <Card>
+      <div>
+        <Title2 block align='center'>{ missionName}</Title2>
+        <Separator />
+        <Subtitle1 block>Rocket Type</Subtitle1>
+        <Body1 block>{ rocketType}</Body1>
+        <Separator />
+        <Subtitle1 block>Motor Type</Subtitle1>
+        <Body1 block>{motorType}</Body1>
+        <Separator />
+        <Subtitle1 block>Avionic Type</Subtitle1>
+        <Body1 block>{avionicType}</Body1>
+      </div>
+      <Label>Message:</Label>
+      <Textarea readOnly={true} value={message}></Textarea>
+      <CompoundButton
+        size='large'
+        appearance='primary'
+        secondaryContent="Unlock before launching"
+        disabled={true}
+      >
+        Launch
+      </CompoundButton>
+      <Button>Unlock</Button>
+    </Card>
+  );
+}
+
+export default MissionPanel;
