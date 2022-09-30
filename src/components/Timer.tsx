224e7928901bb0b619a2997ee89104546111aa92
William Chang <wilicw1346@gmail.com>
2022-09-30 14:28:35 +0800

    Timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Card } from '@fluentui/react-components/unstable';
+import { LargeTitle, Title2, Text } from '@fluentui/react-components';
+
+interface TimerProps {
+  refTime: number;
+}
+
+const currentTime = () => {
+  return new Date().toUTCString()
+}
+
+function Timer({ refTime }: TimerProps) {
+
+  const [displayTime, setDisplayTime] = useState(currentTime);
+
+  useEffect(() => {
+    setInterval(() => {
+      setDisplayTime(() => currentTime())
+    }, 100)
+  }, [])
+
+  const padding = String(refTime.toFixed(0)).padStart(3, '0');
+  const floating = (refTime - parseInt(padding)).toFixed(1).slice(1)
+
+  return (
+    <Card appearance='subtle'>
+      <Text>{displayTime}</Text>
+      <Text align='center'>
+        <LargeTitle block={false}>
+          T+{padding}
+        </LargeTitle>
+        <Title2 block={false}>{floating}s</Title2>
+      </Text>
+    </Card>
+  );
+}
+
+export default Timer;
