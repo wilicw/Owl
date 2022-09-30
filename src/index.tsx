55a901552a7dd1c326a4f29eb62cb040aa7ebe6e
William Chang <wilicw1346@gmail.com>
2022-09-30 14:13:26 +0800

    Use FluentProvider

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <FluentProvider theme={webLightTheme}>
-      <App />
-    </FluentProvider>
-  </React.StrictMode>
+  <FluentProvider theme={webLightTheme}>
+    <App />
+  </FluentProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
