dd068a6e797714a25070e6c34af4ffb651c8dbf6
William Chang <wilicw1346@gmail.com>
2022-10-01 10:46:43 +0800

    Add color prop

---
+++ src/components/DataVisualization.tsx
@@ -0,0 +1,63 @@
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from 'recharts';
+import { Card } from '@fluentui/react-components/unstable';
+import { Subtitle2, Text } from '@fluentui/react-components';
+
+interface DataVisual {
+  time: number;
+  value: number;
+};
+
+interface DataVisualProps {
+  color: string;
+  height: number;
+  chartName: string;
+  unit: string;
+  data: [DataVisual];
+};
+
+function DataVisualization({ color, height, chartName, unit, data }: DataVisualProps) {
+  let displayedValue: string = (data.length ? data[data.length - 1].value : 0).toFixed(4);
+  return (
+    <Card
+      style={{
+        padding: 0,
+        height: height
+      }}
+    >
+      <div
+        style={{
+          marginLeft: "1em",
+          marginRight: "1em",
+          marginTop: ".5em"
+        }}
+      >
+        <Text block>
+          <Subtitle2>
+            {chartName}
+          </Subtitle2>
+          <Subtitle2 style={{ float: 'right', marginLeft: 2 }}>
+            {unit}
+          </Subtitle2>
+          <Subtitle2 style={{ float: 'right' }}>
+            {displayedValue}
+          </Subtitle2>
+        </Text>
+      </div>
+      <ResponsiveContainer
+        width="95%"
+      >
+        <LineChart
+          data={data}
+          margin={{ top: 0, bottom: 0, left: -25, right: 0 }}
+        >
+          <Line type="monotone" dataKey="value" stroke={color} dot={false} />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="time" />
+          <YAxis />
+        </LineChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+}
+
+export default DataVisualization;
