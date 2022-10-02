0dd7dee37195346616da79e4712d8ca2685b98d4
William Chang <wilicw1346@gmail.com>
2022-10-02 13:56:24 +0800

    Share observable

--- src/services/WeatherProvider.ts
+++ src/services/WeatherProvider.ts
@@ -1,4 +1,4 @@
-import { filter, mergeMap, switchMap, timer } from "rxjs"
+import { filter, mergeMap, share, switchMap, timer } from "rxjs"
 
 const weatherApi = "https://volmet.wilicw.dev/metar.json"
 
@@ -6,7 +6,8 @@ const weatherObservable = timer(0, 20 * 1000).pipe(
   switchMap(() => fetch(weatherApi)),
   filter(response => response.status === 200),
   mergeMap(async response => await response.json()),
-  mergeMap(x => x.filter((data: any) => data.station === "RCNN"))
+  mergeMap(x => x.filter((data: any) => data.station === "RCNN")),
+  share(),
 )
 
 export default weatherObservable;
