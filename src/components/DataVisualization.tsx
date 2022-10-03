08154213a04e58c9bbb5e1409e0f8baccedb9948
William Chang <wilicw1346@gmail.com>
2022-10-03 14:31:58 +0800

    Tuning visualization panel

--- src/components/DataVisualization.tsx
+++ src/components/DataVisualization.tsx
@@ -2,7 +2,7 @@ import {
   LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer,
 } from 'recharts';
 import { Card } from '@fluentui/react-components/unstable';
-import { Subtitle2, Text } from '@fluentui/react-components';
+import { Spinner, Subtitle2, Text } from '@fluentui/react-components';
 import IDataVisual from 'interfaces/IDataVisual';
 
 interface DataVisualProps {
@@ -12,6 +12,23 @@ interface DataVisualProps {
   unit: string;
   data: IDataVisual[];
 }
+
+const DataFormatter = (n: number) => {
+  let buffer = n < 0 ? '-' : '';
+  const absN = Math.abs(n);
+  if (absN >= 1000000000000) {
+    buffer += `${(absN / 1000000000000).toString()}T`;
+  } else if (absN >= 1000000000) {
+    buffer += `${(absN / 1000000000).toString()}G`;
+  } else if (absN >= 1000000) {
+    buffer += `${(absN / 1000000).toString()}M`;
+  } else if (absN >= 1000) {
+    buffer += `${(absN / 1000).toString()}K`;
+  } else {
+    buffer += absN.toFixed(1);
+  }
+  return buffer;
+};
 
 function DataVisualization({
   color, height, chartName, unit, data,
@@ -24,40 +41,42 @@ function DataVisualization({
         height,
       }}
     >
-      <div
+      <Text
+        block
         style={{
-          marginLeft: '1em',
-          marginRight: '1em',
-          marginTop: '.5em',
+          paddingLeft: 12,
+          paddingTop: 6,
+          paddingRight: 12,
+          paddingBottom: 0,
         }}
       >
-        <Text block>
-          <Subtitle2>
-            {chartName}
-          </Subtitle2>
-          <Subtitle2 style={{ float: 'right', marginLeft: 2 }}>
-            {unit}
-          </Subtitle2>
-          <Subtitle2 style={{ float: 'right' }}>
-            {displayedValue}
-          </Subtitle2>
-        </Text>
-      </div>
-      <ResponsiveContainer
-        width="95%"
-      >
-        <LineChart
-          data={data}
-          margin={{
-            top: 0, bottom: 0, left: -25, right: 0,
-          }}
-        >
-          <Line type="monotone" dataKey="value" stroke={color} dot={false} />
-          <CartesianGrid stroke="#ccc" />
-          <XAxis dataKey="time" />
-          <YAxis />
-        </LineChart>
-      </ResponsiveContainer>
+        <Subtitle2>
+          {chartName}
+        </Subtitle2>
+        <Subtitle2 style={{ float: 'right', marginLeft: 2 }}>
+          {unit}
+        </Subtitle2>
+        <Subtitle2 style={{ float: 'right' }}>
+          {displayedValue}
+        </Subtitle2>
+      </Text>
+      {
+        data.length ? (
+          <ResponsiveContainer width="100%">
+            <LineChart
+              data={data}
+              margin={{
+                top: 0, bottom: 0, left: 0, right: 12,
+              }}
+            >
+              <Line type="monotone" dataKey="value" stroke={color} dot={false} />
+              <CartesianGrid stroke="#ccc" />
+              <XAxis dataKey="time" allowDecimals={false} type="number" domain={[0, 'dataMax']} tickFormatter={DataFormatter} />
+              <YAxis tickFormatter={DataFormatter} width={40} />
+            </LineChart>
+          </ResponsiveContainer>
+        ) : <Spinner style={{ height: '100%' }} label="Waiting Data..." />
+      }
     </Card>
   );
 }
