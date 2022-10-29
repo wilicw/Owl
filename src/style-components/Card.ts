91832967874d22ce5c5b206981e630a527f1b057
William Chang <wilicw1346@gmail.com>
2022-10-29 22:40:17 +0800

    Add Card component

---
+++ src/style-components/Card.ts
@@ -0,0 +1,9 @@
+import { styled } from '@linaria/react';
+import Container from './Container';
+
+const Card = styled(Container)`
+  border-radius: 5px;
+  box-shadow: 0 0 2px rgba(0,0,0,0.12), 0 2px 4px rgba(0,0,0,0.14);
+`;
+
+export default Card;
