09006ada31f0bb0351ee6cab4898f73b022eef48
William Chang <wilicw1346@gmail.com>
2022-10-30 20:51:24 +0800

    Implement center props

--- src/style-components/Text.ts
+++ src/style-components/Text.ts
@@ -1,10 +1,14 @@
 import { styled } from '@linaria/react';
 
-const Text = styled.p<{ block?: boolean }>`
+const Text = styled.p<{
+  block?: boolean,
+  align?: string
+}>`
   margin: 4px;
   font-weight: 600;
   font-size: 1.05em;
   display: ${(props) => (props.block ? 'block' : 'inline-block')};
+  text-align: ${(props) => props.align || 'left'}
 `;
 
 export default Text;
