a910d80bbdd13fdd45ac89c43eafe99f1a02cfcc
William Chang <wilicw1346@gmail.com>
2022-11-13 16:03:57 +0800

    Add Separator components

---
+++ src/style-components/Separator.ts
@@ -0,0 +1,10 @@
+import { styled } from '@linaria/react';
+
+const Separator = styled.hr`
+  margin: .7em;
+  background-color: #DDD;
+  height: 1px;
+  border: none;
+`;
+
+export default Separator;
