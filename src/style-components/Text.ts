df1189ab3a325ab3a633c7302e516386f84f6cdd
William Chang <wilicw1346@gmail.com>
2022-11-13 15:52:51 +0800

    Text components blod props

--- src/style-components/Text.ts
+++ src/style-components/Text.ts
@@ -2,10 +2,11 @@ import { styled } from '@linaria/react';
 
 const Text = styled.p<{
   block?: boolean,
-  align?: string
+  align?: string,
+  blod?: boolean,
 }>`
   margin: 4px;
-  font-weight: 600;
+  font-weight: ${(props) => (props.blod ? 600 : 400)};
   font-size: 1.05em;
   display: ${(props) => (props.block ? 'block' : 'inline-block')};
   text-align: ${(props) => props.align || 'left'}
