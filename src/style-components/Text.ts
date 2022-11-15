6d44ef98aa42920fd8f214fefb87108d35f0d160
William Chang <wilicw1346@gmail.com>
2022-11-15 10:19:20 +0800

    Change text size

--- src/style-components/Text.ts
+++ src/style-components/Text.ts
@@ -7,7 +7,6 @@ const Text = styled.p<{
 }>`
   margin: 4px;
   font-weight: ${(props) => (props.blod ? 600 : 400)};
-  font-size: 1.05em;
   display: ${(props) => (props.block ? 'block' : 'inline-block')};
   text-align: ${(props) => props.align || 'left'}
 `;
