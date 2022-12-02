5cb9e1e4c9e6ae38d1b3210c00886bbe786a1bf7
William Chang <wilicw1346@gmail.com>
2022-12-02 09:38:55 +0800

    Disable stream and nostream when sending command

--- src/components/MissionPanel.tsx
+++ src/components/MissionPanel.tsx
@@ -62,9 +62,7 @@ function MissionPanel({
   };
 
   const commandAction = (e: React.FormEvent<HTMLFormElement>) => {
-    sendMessage$.next(new TextEncoder().encode('nostream\n'));
     sendMessage$.next(new TextEncoder().encode(`${command}\n`));
-    sendMessage$.next(new TextEncoder().encode('stream\n'));
     setCommand(() => '');
     e.preventDefault();
   };
