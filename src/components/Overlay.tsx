b55a64d7ad42c88cbe18d10fe2adc1219f351718
William Chang <wilicw1346@gmail.com>
2022-12-04 23:01:16 +0800

    Implement overlay component

---
+++ src/components/Overlay.tsx
@@ -0,0 +1,37 @@
+import Overlay from '@/style-components/Overlay';
+
+interface OverlayCardProp {
+  close: () => void,
+  children: React.ReactNode
+}
+
+function OverlayCard({ close, children }:OverlayCardProp) {
+  return (
+    <div
+      role="presentation"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        zIndex: 9999,
+        backgroundColor: 'rgba(0,0,0,0.2)',
+      }}
+      onClick={close}
+      onKeyDown={(e) => e.stopPropagation()}
+    >
+      <Overlay
+        onClick={(e) => e.stopPropagation()}
+        style={{
+          width: window.innerWidth * 0.8,
+          height: window.innerHeight * 0.8,
+        }}
+      >
+        {children}
+      </Overlay>
+    </div>
+  );
+}
+
+export default OverlayCard;
---
+++ src/style-components/Overlay.ts
@@ -0,0 +1,17 @@
+import { styled } from '@linaria/react';
+
+const Overlay = styled.div`
+  background-color: white;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  z-index: 999;
+  padding: 30px;
+  background-color: #FFF;
+  padding: 12;
+`;
+
+export default Overlay;
